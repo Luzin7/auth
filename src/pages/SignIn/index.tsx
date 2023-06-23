31a899d18bfafedcaddd794b97ad17198b757ce2
Luzin7 <lvictor.dutra@gmail.com>
2023-06-23 17:01:24 -0300

    style(index.tsx): ajusta largura

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,7 +3,7 @@ import Form from "../../components/Form/Form";
 
 function SignIn() {
   return (
-    <section className="w-full mx-auto h-screen px-8 py-16 bg-gray-700 xl:px-8 flex justify-center items-center md:flex-row">
+    <section className="w-screen mx-auto h-screen px-8 py-16 bg-gray-700 xl:px-8 flex justify-center items-center md:flex-row">
       <div className="max-w-7xl flex justify-center items-center md:flex-row">
         <Hero />
         <Form />
